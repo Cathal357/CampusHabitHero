cc6f10baeed0e2fc2336d445a46b30dda9d0a738
Cathal357 <cathalmcfadden03@gmail.com>
2024-03-12 13:48:04 +0000

    updating footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,26 +5,27 @@ const Footer = () => {
     <div>
       
 
-<footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
-    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
-    </span>
-    <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-        <li>
-            <a href="#" class="hover:underline me-4 md:me-6">About</a>
-        </li>
-        <li>
-            <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
-        </li>
-        <li>
-            <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
-        </li>
-        <li>
-            <a href="#" class="hover:underline">Contact</a>
-        </li>
-    </ul>
-    </div>
+<footer class="text-gray-600 bg-gray-100 flex justify-end items-center p-4">
+  <ul class="list-none mr-4">
+    <li class="inline mr-4">
+      <a href="#" class="text-decoration-none hover:text-gray-700">Features</a>
+    </li>
+    <li class="inline mr-4">
+      <a href="#" class="text-decoration-none hover:text-gray-700">Products</a>
+    </li>
+    <li class="inline mr-4">
+      <a href="#" class="text-decoration-none hover:text-gray-700">FAQs</a>
+    </li>
+    <li class="inline mr-4">
+      <a href="#" class="text-decoration-none hover:text-gray-700">Contact Us</a>
+    </li>
+    <li class="inline">
+      <a href="#" class="text-decoration-none hover:text-gray-700">Placeholder Hamburger For Footer</a>
+    </li>
+  </ul>
 </footer>
+
+
 
     </div>
   );
